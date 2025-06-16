cc3af58a91c7fedd7bac6baada32b7896df2c240
Abdul Mecit Usda <92247286+mecitusda@users.noreply.github.com>
2025-06-16 17:49:00 +0300

    router fix

--- Scriber-Backend/routes/web/web.js
+++ Scriber-Backend/routes/web/web.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 
 
-app.get('/contact', (req, res) => {
+router.get('/contact', (req, res) => {
   res.render('pages/contact', { title: 'About Scriber' });
 });
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.render('pages/index', { title: 'Scriber' });
 });
 
